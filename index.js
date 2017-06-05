99d8027ed566c8e9fce9a911bfd5e0163151ab21
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-05 17:31:48 -0400

    Add second input and color-changing paragraph

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value;
+    heading.textContent = 'Hello, ' + f.personName.value
+                            + ', you like the color ' + f.favoriteColor.value.toLowerCase() 
+                            + '!';
+    const paragraph = document.querySelector('p#output');                         
+    paragraph.textContent = 'Hey ' + f.personName.value +
+                                                    '!' + ' You like the color ' + 
+                                                    f.favoriteColor.value.toLowerCase() + '. Good taste!';
+    paragraph.style.backgroundColor = f.favoriteColor.value.toLowerCase();
+
 }
 
 personForm.addEventListener('submit', handleSubmit);
