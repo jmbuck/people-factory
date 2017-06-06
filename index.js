045ee928e7477912bc904abfefc5740877f500b8
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-06 09:24:49 -0400

    Add last name and allow for white paragraph text

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const personForm = document.querySelector('#personForm');
 
 function handleSubmit(ev) {
     ev.preventDefault()
-    const f = ev.target
-    const heading = document.querySelector('h1')
-    heading.textContent = 'Hello, ' + f.personName.value
-                            + ', you like the color ' + f.favoriteColor.value.toLowerCase() 
-                            + '!';
-    const paragraph = document.querySelector('p#output');                         
-    paragraph.textContent = 'Hey ' + f.personName.value +
+    
+    const f = ev.target;
+    const heading = document.querySelector('h1');
+    const paragraph = document.querySelector('p#output');
+    let favoriteColor = f.favoriteColor.value.toLowerCase(); 
+    
+    //Modify heading and paragraph text/background color
+    heading.textContent = f.firstName.value + ' ' + f.lastName.value;                      
+    paragraph.textContent = 'Hey ' + f.firstName.value +
                                                     '!' + ' You like the color ' + 
-                                                    f.favoriteColor.value.toLowerCase() + '. Good taste!';
-    paragraph.style.backgroundColor = f.favoriteColor.value.toLowerCase();
+                                                     favoriteColor + '. Good taste!';
+    paragraph.style.backgroundColor = favoriteColor;
+    
+    //Modify text to be easier to read if necessary
+    if(favoriteColor == 'black' || favoriteColor == 'green')
+        paragraph.style.color = 'white';
+    else
+        paragraph.style.color = 'black';
 
 }
 
