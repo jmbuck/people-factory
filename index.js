248a6f6b94b2ca863a1c6b2f032e301e941927de
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-06 14:05:25 -0400

    Add name to details

--- index.js
+++ index.js
@@ -4,24 +4,27 @@ function handleSubmit(ev) {
     ev.preventDefault()
     
     const f = ev.target;
+    const details = document.querySelector('#details');
     const heading = document.querySelector('h1');
     const paragraph = document.querySelector('p#output');
     let favoriteColor = f.favoriteColor.value.toLowerCase().replace(' ', ''); 
+    const name = f.firstName.value;
     
+    details.textContent += name;
     //Modify heading and paragraph text/background color
     heading.textContent = f.firstName.value + ' ' + f.lastName.value;                      
-    paragraph.textContent = 'Hey ' + f.firstName.value +
-                                                    '!' + ' You like the color ' + 
-                                                     f.favoriteColor.value.toLowerCase() + '. Good taste!';
-    paragraph.style.backgroundColor = favoriteColor;
+    // paragraph.textContent = 'Hey ' + f.firstName.value +
+    //                                                 '!' + ' You like the color ' + 
+    //                                                  f.favoriteColor.value.toLowerCase() + '. Good taste!';
+    // paragraph.style.backgroundColor = favoriteColor;
     
-    //Modify text to be easier to read if favoriteColor is dark
-    if(favoriteColor == 'black' || favoriteColor == 'green' 
-    || favoriteColor.includes('dark') || favoriteColor == 'brown'
-    || favoriteColor == 'purple' || favoriteColor == 'indigo')
-        paragraph.style.color = 'white';
-    else
-        paragraph.style.color = 'black';
+    // //Modify text to be easier to read if favoriteColor is dark
+    // if(favoriteColor == 'black' || favoriteColor == 'green' 
+    // || favoriteColor.includes('dark') || favoriteColor == 'brown'
+    // || favoriteColor == 'purple' || favoriteColor == 'indigo')
+    //     paragraph.style.color = 'white';
+    // else
+    //     paragraph.style.color = 'black';
 
 }
 
