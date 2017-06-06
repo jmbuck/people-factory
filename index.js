5d03b99b2ae1073e8ae8b9b654c9b94cc0a51bfc
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-06 09:36:51 -0400

    Refine paragraph color-changing

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ function handleSubmit(ev) {
     const f = ev.target;
     const heading = document.querySelector('h1');
     const paragraph = document.querySelector('p#output');
-    let favoriteColor = f.favoriteColor.value.toLowerCase(); 
+    let favoriteColor = f.favoriteColor.value.toLowerCase().replace(' ', ''); 
     
     //Modify heading and paragraph text/background color
     heading.textContent = f.firstName.value + ' ' + f.lastName.value;                      
     paragraph.textContent = 'Hey ' + f.firstName.value +
                                                     '!' + ' You like the color ' + 
-                                                     favoriteColor + '. Good taste!';
+                                                     f.favoriteColor.value.toLowerCase() + '. Good taste!';
     paragraph.style.backgroundColor = favoriteColor;
     
     //Modify text to be easier to read if necessary
-    if(favoriteColor == 'black' || favoriteColor == 'green')
+    if(favoriteColor == 'black' || favoriteColor == 'green' 
+    || favoriteColor.includes('dark') || favoriteColor == 'brown')
         paragraph.style.color = 'white';
     else
         paragraph.style.color = 'black';
