2ffa732186ba16b1a613b2120518467a7ffc19e0
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-06 14:38:16 -0400

    Display favorite colora and age in details

--- index.js
+++ index.js
@@ -7,12 +7,23 @@ function handleSubmit(ev) {
     const details = document.querySelector('#details');
     const heading = document.querySelector('h1');
     const paragraph = document.querySelector('p#output');
-    const favoriteColor = f.favoriteColor.value.toLowerCase().replace(' ', ''); 
+    //const favoriteColor = f.favoriteColor.value.toLowerCase().replace(' ', ''); 
     const name = f.firstName.value;
-   
-    const bold = document.createElement('strong');
-    bold.textContent = name
-    details.appendChild(bold);
+    const favoriteColor = f.favoriteColor.value;
+    const age = f.age.value;
+
+    // const bold = document.createElement('strong');
+    // bold.textContent = name
+    // details.appendChild(bold);
+
+    details.innerHTML = `
+        <ul>
+            <li>Name: ${name}</li>
+            <li>Favorite Color: ${favoriteColor}</li>
+            <li>Age: ${age}</li>
+        </ul>
+    `
+
     // //Modify heading and paragraph text/background color
     // heading.textContent = f.firstName.value + ' ' + f.lastName.value;                      
     // paragraph.textContent = 'Hey ' + f.firstName.value +
