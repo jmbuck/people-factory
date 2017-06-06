937f8b5c3316f0c40cd5f5254c49e1a0a981d249
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-06 14:14:42 -0400

    Append name with proper DOM element

--- index.js
+++ index.js
@@ -7,24 +7,26 @@ function handleSubmit(ev) {
     const details = document.querySelector('#details');
     const heading = document.querySelector('h1');
     const paragraph = document.querySelector('p#output');
-    let favoriteColor = f.favoriteColor.value.toLowerCase().replace(' ', ''); 
+    const favoriteColor = f.favoriteColor.value.toLowerCase().replace(' ', ''); 
     const name = f.firstName.value;
-    
-    details.innerHTML += `<strong>${name}</strong>`;
+   
+    const bold = document.createElement('strong');
+    bold.textContent = name
+    details.appendChild(bold);
     //Modify heading and paragraph text/background color
     heading.textContent = f.firstName.value + ' ' + f.lastName.value;                      
-    // paragraph.textContent = 'Hey ' + f.firstName.value +
-    //                                                 '!' + ' You like the color ' + 
-    //                                                  f.favoriteColor.value.toLowerCase() + '. Good taste!';
-    // paragraph.style.backgroundColor = favoriteColor;
+    paragraph.textContent = 'Hey ' + f.firstName.value +
+                                                    '!' + ' You like the color ' + 
+                                                     f.favoriteColor.value.toLowerCase() + '. Good taste!';
+    paragraph.style.backgroundColor = favoriteColor;
     
-    // //Modify text to be easier to read if favoriteColor is dark
-    // if(favoriteColor == 'black' || favoriteColor == 'green' 
-    // || favoriteColor.includes('dark') || favoriteColor == 'brown'
-    // || favoriteColor == 'purple' || favoriteColor == 'indigo')
-    //     paragraph.style.color = 'white';
-    // else
-    //     paragraph.style.color = 'black';
+    //Modify text to be easier to read if favoriteColor is dark
+    if(favoriteColor == 'black' || favoriteColor == 'green' 
+    || favoriteColor.includes('dark') || favoriteColor == 'brown'
+    || favoriteColor == 'purple' || favoriteColor == 'indigo')
+        paragraph.style.color = 'white';
+    else
+        paragraph.style.color = 'black';
 
 }
 
