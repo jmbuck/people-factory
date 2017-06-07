c39303e53443d442b64a6fe2c25dfa35a06a0e6f
Jordan Buckmaster <buckmast@purdue.edu>
2017-06-07 14:57:41 -0400

    Wrap code in object

--- index.js
+++ index.js
@@ -1,82 +1,86 @@
-const personForm = document.querySelector('#personForm');
+const peopleFactory = {
+    init: function(formSelector) {
+        const personForm = document.querySelector(formSelector);
+        personForm.addEventListener('submit', this.handleSubmit.bind(this));
+    },
 
-function handleSubmit(ev) {
-    ev.preventDefault()
-    
-    const f = ev.target;
-    const details = document.querySelector('#details');
-    const heading = document.querySelector('h1');
-    const paragraph = document.querySelector('p#output');
-    //const favoriteColor = f.favoriteColor.value.toLowerCase().replace(' ', ''); 
+    handleSubmit: function(ev) {
+        ev.preventDefault();
+        
+        const f = ev.target;
+        const details = document.querySelector('#details');
+        const heading = document.querySelector('h1');
+        const paragraph = document.querySelector('p#output');
+        //const favoriteColor = f.favoriteColor.value.toLowerCase().replace(' ', ''); 
+        const person = {'Name': f.firstName.value, 
+                        'Favorite Color': this.renderColor(f.favoriteColor.value).outerHTML, 
+                        'Age': f.age.value,};
 
-    const person = {'Name': f.firstName.value, 
-                    'Favorite Color': renderColor(f.favoriteColor.value).outerHTML, 
-                    'Age': f.age.value,};
+        details.appendChild(this.createList(person));
 
-    details.appendChild(createList(person));
+        // details.innerHTML = `
+        //     <ul>
+        //         <li>Name: ${name}</li>
+        //         <li>Favorite Color: ${colorDiv}</li>
+        //         <li>Age: ${age}</li>
+        //     </ul>
+        // `;
 
-    // details.innerHTML = `
-    //     <ul>
-    //         <li>Name: ${name}</li>
-    //         <li>Favorite Color: ${colorDiv}</li>
-    //         <li>Age: ${age}</li>
-    //     </ul>
-    // `;
+        // //Modify heading and paragraph text/background color
+        // heading.textContent = f.firstName.value + ' ' + f.lastName.value;                      
+        // paragraph.textContent = 'Hey ' + f.firstName.value +
+        //                                                 '!' + ' You like the color ' + 
+        //                                                  f.favoriteColor.value.toLowerCase() + '. Good taste!';
+        // paragraph.style.backgroundColor = favoriteColor;
+        
+        // //Modify text to be easier to read if favoriteColor is dark
+        // if(favoriteColor == 'black' || favoriteColor == 'green' 
+        // || favoriteColor.includes('dark') || favoriteColor == 'brown'
+        // || favoriteColor == 'purple' || favoriteColor == 'indigo')
+        //     paragraph.style.color = 'white';
+        // else
+        //     paragraph.style.color = 'black';
 
-    // //Modify heading and paragraph text/background color
-    // heading.textContent = f.firstName.value + ' ' + f.lastName.value;                      
-    // paragraph.textContent = 'Hey ' + f.firstName.value +
-    //                                                 '!' + ' You like the color ' + 
-    //                                                  f.favoriteColor.value.toLowerCase() + '. Good taste!';
-    // paragraph.style.backgroundColor = favoriteColor;
-    
-    // //Modify text to be easier to read if favoriteColor is dark
-    // if(favoriteColor == 'black' || favoriteColor == 'green' 
-    // || favoriteColor.includes('dark') || favoriteColor == 'brown'
-    // || favoriteColor == 'purple' || favoriteColor == 'indigo')
-    //     paragraph.style.color = 'white';
-    // else
-    //     paragraph.style.color = 'black';
+    },
 
-}
+    renderColor: function (color) {
+        const div = document.createElement('div');
+        div.style.backgroundColor = color;
+        div.style.height = '50px';
+        div.style.width = '100px';
 
-function renderColor(color) {
-    const div = document.createElement('div');
-    div.style.backgroundColor = color;
-    div.style.height = '50px';
-    div.style.width = '100px';
+        return div;
+    },
 
-    return div;
-}
+    createListItem: function(label, value) {
+        const item = document.createElement('li');
+        item.innerHTML = `${label}: ${value}`;
+        return item;
+    },
 
-function createListItem(label, value) {
-    const item = document.createElement('li');
-    item.innerHTML = `${label}: ${value}`;
-    return item;
-}
+    createList: function(personData) {
+    //    const list = document.createElement('ul');
+    //    const nameItem = document.createElement('li');
+    //    const colorItem = document.createElement('li');
+    //    const ageItem = document.createElement('li');
 
-function createList(personData) {
-//    const list = document.createElement('ul');
-//    const nameItem = document.createElement('li');
-//    const colorItem = document.createElement('li');
-//    const ageItem = document.createElement('li');
+    //    nameItem.textContent = `Name: ${name}`;
+    //    colorItem.innerHTML = `Favorite Color: ${renderColor(color).outerHTML}`;
+    //    ageItem.textContent = `Age: ${age}`;
 
-//    nameItem.textContent = `Name: ${name}`;
-//    colorItem.innerHTML = `Favorite Color: ${renderColor(color).outerHTML}`;
-//    ageItem.textContent = `Age: ${age}`;
+    //    list.appendChild(nameItem);
+    //    list.appendChild(colorItem);
+    //    list.appendChild(ageItem);
+    //    return list;
+        const list = document.createElement('ul');
+       
+        Object.keys(personData).map((label) => {
+            const item = this.createListItem(label, personData[label]);
+            list.appendChild(item);
+        });
 
-//    list.appendChild(nameItem);
-//    list.appendChild(colorItem);
-//    list.appendChild(ageItem);
-//    return list;
-    const list = document.createElement('ul');
-    
-    Object.keys(personData).map(function(label) {
-        const item = createListItem(label, personData[label]);
-        list.appendChild(item);
-    });
+        return list;
+    },
+};
 
-    return list;
-}
-
-personForm.addEventListener('submit', handleSubmit);
+peopleFactory.init('#personForm');
